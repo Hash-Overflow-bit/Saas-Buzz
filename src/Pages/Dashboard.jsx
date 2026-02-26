d51e6470a2c27c13b3859a0376f946c0799e8a14
devwithhahsir <f2022065252@umt.edu.pk>
2026-02-26 15:28:32 +0500

    Dashboard is ready

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaArrowRight } from "react-icons/fa";
 import BackgroundTemplate from "../components/BackgroundTemplate/BackgroundTemplate";
 import "./Dashboard.css";
 import { useState } from "react";
@@ -7,6 +8,7 @@ function Dashboard() {
   const [open, setOpen] = useState(false);
   return (
     <div className="dashboard-main-container">
+
       {/* Header */}
       <div className="dashboard-header">
         <div className="dashboard-header-logo">
@@ -50,21 +52,127 @@ function Dashboard() {
         </div>
       </div>
 
-      <div className="BackgroundTemplateOnDashboard">
-        <BackgroundTemplate />
-      </div>
+     
 
-       {/* Dashboard content */}
+      {/* Dashboard content */}
       <div className="dashboard-container">
+        <div className="BackgroundTemplateOnDashboard">
+          <BackgroundTemplate />
+        </div>
+        
         {/* welcome back User */}
-        <div className="greeting-user"><p>Hi, Gohar</p></div>
+        <div className="greeting-user">
+          <p>Hi, Gohar</p>
+        </div>
+
+        {/* Dashboard input */}
+
+        <div className="dashboard-input">
+          <div className="input-wrapper">
+            <input type="text" placeholder="Enter the Address of the website" />
+            <button className="arrow-btn">
+              <img src="public\Images\arrow-btn.svg" alt="" />
+            </button>
+          </div>
+        </div>
+
+        {/* Recent searches/////// recent website searches */}
+
+        <div className="recent-search-container">
+
+          <div className="recent-search-website-container">
+            <div className="search-web-result">
+              <div className="web-name">
+                {" "}
+                <span className="search-web-icon">
+                  <img src="public\Images\web icon.svg" alt="" />
+                </span>
+                Buzz Interactive
+              </div>
+              <div className="result">Fair <span><img src="public\Images\gradient-icon.svg" alt="" /></span></div>
+            </div>
+
+            <div className="leads-report-container">
+              <div className="leads">100 leads</div>
+              <div className="report">
+                <button>
+                  View Report{" "}
+                  <FaArrowRight
+                    style={{
+                      marginLeft: "6px",
+                      fontSize: "15px",
+                      verticalAlign: "middle",
+                    }}
+                  />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="recent-search-website-container">
+            <div className="search-web-result">
+              <div className="web-name">
+                {" "}
+                <span className="search-web-icon">
+                  <img src="public\Images\web icon.svg" alt="" />
+                </span>
+                Buzz Interactive
+              </div>
+              <div className="result">Fair <span><img src="public\Images\gradient-icon.svg" alt="" /></span></div>
+            </div>
+
+            <div className="leads-report-container">
+              <div className="leads">100 leads</div>
+              <div className="report">
+                <button>
+                  View Report{" "}
+                  <FaArrowRight
+                    style={{
+                      marginLeft: "6px",
+                      fontSize: "15px",
+                      verticalAlign: "middle",
+                    }}
+                  />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="recent-search-website-container">
+            <div className="search-web-result">
+              <div className="web-name">
+                {" "}
+                <span className="search-web-icon">
+                  <img src="public\Images\web icon.svg" alt="" />
+                </span>
+                Buzz Interactive
+              </div>
+              <div className="result">Fair <span><img src="public\Images\gradient-icon.svg" alt="" /></span></div>
+            </div>
+
+            <div className="leads-report-container">
+              <div className="leads">100 leads</div>
+              <div className="report">
+                <button>
+                  View Report{" "}
+                  <FaArrowRight
+                    style={{
+                      marginLeft: "6px",
+                      fontSize: "15px",
+                      verticalAlign: "middle",
+                    }}
+                  />
+                </button>
+              </div>
+            </div>
+          </div>
+
+
+
+
+          
+
+
+        </div>
       </div>
-
-
-
-
-
-
     </div>
   );
 }
